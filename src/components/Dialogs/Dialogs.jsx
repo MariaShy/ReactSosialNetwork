b163aefcddc0e2bfa565f6d831e868a3ba7dd6d9
Maria Shy <24011989m@mail.ru>
2020-11-29 16:45:42 +0200

    L21, Dialogs layout

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,16 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog + ' ' + s.active}>Mike</div>
+                <div className={s.dialog}>Nick</div>
+                <div className={s.dialog}>Lukas</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}> How are you? </div>
+            </div>
         </div>
     );
 }
