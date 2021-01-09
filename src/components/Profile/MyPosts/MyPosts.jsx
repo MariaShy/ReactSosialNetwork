ad1d564fe8ae8abc5b4078fd7f577af2096fbb56
Maria Shy <24011989m@mail.ru>
2021-01-09 15:15:40 +0300

    L31 React.createRef for textarea, onClick for button

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,17 +7,24 @@ const MyPosts = (props) => {
 
     let posts = props.postsData.map(
         p => <Post message={p.message} likeCount={p.likeCount}/>
-    )
+    );
+
+    let newPostElement = React.createRef();
+
+    let addPost = ()=> {
+        let text = newPostElement.current.value;
+        alert(text);
+    }
 
     return (
     <div className={s.postsBlock}>
         <h3>My posts</h3>
         <div>
             <div>
-                <textarea></textarea>
+                <textarea ref={newPostElement}></textarea>
             </div>
             <div>
-                <button>Add post</button>
+                <button onClick={ addPost }>Add post</button>
             </div>
         </div>
         <div className={s.posts}>
